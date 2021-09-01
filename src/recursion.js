458ac7ee602249ae60431182929961fa14e181f5
jaimienakayama <jaimie.nakayama@icloud.com>
2021-09-01 14:27:48 -0400

    sums all integers below a given number using recursion

--- src/recursion.js
+++ src/recursion.js
@@ -89,6 +89,28 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+
+  // boolean flag
+  var resultIsNegative = false;
+
+  // edge case
+  if (n < 0) {
+    n = Math.abs(n);
+    resultIsNegative = true;
+  }
+
+  // base case
+  if (n === 0) {
+    return 0;
+  }
+  var result = (n - 1) + sumBelow(n - 1)
+
+  if (resultIsNegative) {
+    return -result;
+  } else {
+    return result;
+  }
+
 };
 
 // 6. Get the integers within a range (x, y).
